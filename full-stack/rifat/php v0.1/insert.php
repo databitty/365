d6dee3e65f26b1f5e05ab496179e2d4d655bf0ba
Ahsanul Haque Rifat <Rifat266@gmail.com>
2017-01-10 14:48:14 +0600

    Update insert.php

--- full-stack/rifat/php v0.1/insert.php
+++ full-stack/rifat/php v0.1/insert.php
@@ -31,27 +31,40 @@ mysqli_close($con);
 	<link rel="stylesheet" type="text/css" href="style2.css">
 </head>
 <body>
-	<div id="h"><h1>Add New Member</h1> </div>
+<div class="header">
+  <a href="admin.php"> LogOut</a>
+</div>
+
+<div class="center">
+  <ul class="menu">
+    <li><a style="margin-left:270px" href="home.php">Home</a></li>
+    <li><a href="insert.php">Insert</a></li>
+    <li><a href="update.php">Updat</a></li>
+    <li><a href="delete.php">Delete</a></li>
+    <li><a href="search.php">Search</a></li>
+  </ul>
+</div>
+<div id="h"><h1>Add New Member</h1> </div>
 <form id="table" action="insert.php" method="POST">
 	<table align="center" id="tb">
 	   <tr>
-	        <td><input type="text" name="fname" placeholder="First Name" required /></td>
+		<td><input type="text" name="fname" placeholder="First Name" required /></td>
 	   </tr>
 	   <tr>
-	        <td><input type="text" name="lname" placeholder="Last Name" required /></td>
+		<td><input type="text" name="lname" placeholder="Last Name" required /></td>
 	   </tr>
 
 	   <tr>
-	        <td><input type="text" name="name" placeholder="User Name" required /></td>
+		<td><input type="text" name="name" placeholder="User Name" required /></td>
 	   </tr>
 	   <tr>
-	        <td><input type="email" name="email" placeholder="Your Email" required /></td>
+		<td><input type="email" name="email" placeholder="Your Email" required /></td>
 	   </tr>
 	   <tr>
-	        <td><input type="password" name="pass" placeholder="Your Password" required /></td>
+		<td><input type="password" name="pass" placeholder="Your Password" required /></td>
 	   </tr>
 	   <tr>
-	        <td><button type="submit" class="btn btn-primary btn-block btn-large" name="submit">SUBMIT</button></td>
+		<td><button type="submit" class="btn btn-primary btn-block btn-large" name="submit">SUBMIT</button></td>
 	   </tr>
 	</table>
 </form>
